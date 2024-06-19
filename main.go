31851f90a09f919cedf818a8da82bba842b6fbfc
Asad Mahmood <asadmhmd1234@gmail.com>
2024-06-19 22:44:30 +0530

    add image file

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import "net/http"
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Handler: serveMux,
 		Addr:    "localhost:8080",
 	}
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	server.ListenAndServe()
 }
